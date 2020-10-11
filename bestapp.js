0ca8c7d99ed543b44f4c842765c61accb4514886
AYUSHI-B83 <65790730+AYUSHI-B83@users.noreply.github.com>
2020-10-12 04:37:09 +0530

    Update bestapp.js

--- bestapp.js
+++ bestapp.js
@@ -1,4 +1,4 @@
-$(".inner-switch" ).on("click", function() {
+$( ".inner-switch" ).on("click", function() {
     if( $( "body" ).hasClass( "dark" )) {
       $( "body" ).removeClass( "dark" );
       $( ".inner-switch" ).text( "OFF" );
